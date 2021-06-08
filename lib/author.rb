7fe2893d561b76507e402bd997f272dd0ecaa66b
DJ Taylor <dtaylor88@gmail.com>
2021-06-08 23:01:53 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -11,6 +11,12 @@ class Author
     @@all 
   end
   
+  def add_post(post)
+    post.author = self
+  end
+  
+  def add_post_by_title(post)
+  
   def posts
     Post.all.select {|post| post.author == self}
   end 
